d173eef062d2127037ce1934bec012bcb544e262
Pete Mason <pete.dev@robot9.uk>
2024-06-07 18:16:57 +0100

    Create and poplate grid
    Create the basic grid; the form allows the user to create a new grid on demand, specifying the number of squares/cells per side.

--- eas.js
+++ eas.js
@@ -1,29 +1,49 @@
+// Default values
+const defaultCells = 4;
+
+//
 const numberCells = document.querySelector("#numberCells");
 const createButton = document.querySelector("button#create");
 const grid = document.querySelector("#grid");
 
-const defaultCells = 16;
+//init code
 numberCells.value = defaultCells;
+createGrid(defaultCells);
+createButton.addEventListener("click", handleCreateClick);
 
 //handle the click
-const handleCreateClick = () => {
+function handleCreateClick() {
   //make sure numberCells is a sensible value
+  //TODO
+  const userCells = numberCells.value;
+
   //create the grid again
-};
+  createGrid(userCells);
+}
 
 // create the canvas -> a grid of numCells x numCells
 // i.e. a total of numCells^2 cells
-const createGrid = function (numCells) {
+function createGrid(numCells) {
+  grid.replaceChildren();
   const cells = []; //array of nodes
+  let newRow, newCell;
 
   // create the array
+  for (let i = 0; i < numCells; i++) {
+    newRow = document.createElement("div");
+    newRow.classList.add("row");
+    for (let j = 0; j < numCells; j++) {
+      newCell = document.createElement("div");
+      //newCell.textContent = `${i}x${j}`;
+      newRow.appendChild(newCell);
+    }
+    grid.appendChild(newRow);
+  }
 
   //do we need to manually create IDs?
 
   //probably ought to
-
-  grid.replaceChildren(cells);
-};
+}
 
 const handleCellSelect = function (e) {
   // determine the target
